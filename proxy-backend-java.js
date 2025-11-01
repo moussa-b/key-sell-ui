0f5f299c309215e211e04a03d75eb14b8951f16c
moussa <moussa@bdmz.fr>
2025-11-01 14:07:52 +0100

    add script for java backend

--- proxy-backend-java.js
+++ proxy-backend-java.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'https://mbo.helioho.st',
+    target: 'http://localhost:8080',
     secure: false,
     changeOrigin: true,
     ws: true,
